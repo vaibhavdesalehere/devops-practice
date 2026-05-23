cc3f82f620ddc075929fbfed80da7efc0f1fbe53
Vaibhav <iamvaibhavdesale@gmail.com>
2026-05-23 04:07:41 +0000

    first commit - added scripts & projects files

---
+++ app/myscript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "==============================="
+echo "System Information Script"
+echo "==============================="
+echo "Usermame : $USER "
+echo "Current directory \ : $PWD"
+echo "Todays's date : $(date)"
+echo "Server hostname : $(hostname)"
+echo "Disk usage : "
+df -h /
+echo "Memory usage : "
+free -h
+echo "==============================="
+
